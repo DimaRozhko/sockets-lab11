c7fd62bb9ba3cd66f4de4540df3bcb98cdf567b0
rozhko.dmytro <dimarozhko2000@gmail.com>
2022-04-09 20:49:36 +0300

    template sync datagram and raw

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # sockets-lab11
 Sockets lab11
 
+g++ -o datagram/synchronised/datagram-client-sinc.obj datagram/synchronised/datagram-client-sinc.cpp 
+./datagram/synchronised/datagram-client-sinc.obj
+
+g++ -o datagram/synchronised/datagram-server-sinc.obj datagram/synchronised/datagram-server-sinc.cpp
+./datagram/synchronised/datagram-server-sinc.obj
+
+g++ -o raw/synchronised/raw-client-sinc.obj raw/synchronised/raw-client-sinc.cpp
+./raw/synchronised/raw-client-sinc.obj 
+
+g++ -o raw/synchronised/raw-server-sinc.obj raw/synchronised/raw-server-sinc.cpp 
+./raw/synchronised/raw-server-sinc.obj
+
 g++ -o streaming/synchronised/streaming-client-sinc.obj ./streaming/synchronised/streaming-client-sinc.cpp
 ./streaming/synchronised/streaming-client-sinc.obj
 
---
+++ datagram/synchronised/datagram-client-sinc.cpp
@@ -0,0 +1,31 @@
+#include "../../common/constant.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main (int argc, char* argv[]) {
+    int socket_client = socket(AF_INET, SOCK_DGRAM,0);
+    if (socket_client < 0) {
+        perror("ERROR: incorrect socket\n");
+        exit(1);
+    }
+    sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_port = htons(8080);
+    address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    if (connect(socket_client, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("ERROR: can't connect to server\n");
+        exit(2);
+    }
+    char message[MESSAGE_LEN];
+    strcpy(message, argc <= 1 ? "Datagram sinchronised message" : argv[1]);
+    send(socket_client, message, MESSAGE_LEN, 0);
+    char recvest[MESSAGE_LEN];
+    recv(socket_client, recvest, sizeof(MESSAGE_LEN), 0);
+    close(socket_client);
+    return 0;
+}
